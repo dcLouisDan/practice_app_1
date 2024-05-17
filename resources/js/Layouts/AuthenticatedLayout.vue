ee67869c7f03fc9438fee21d4d3da752b3d886e7
Dan Louis Dela Cruz <louisdc002@gmail.com>
2024-05-18 07:56:11 +0800

    Initial commit

---
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from "vue";
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import Dropdown from "@/Components/Dropdown.vue";
+import DropdownLink from "@/Components/DropdownLink.vue";
+import NavLink from "@/Components/NavLink.vue";
+import ResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
+import { Link } from "@inertiajs/vue3";
+
+const showingNavigationDropdown = ref(false);
+</script>
+
+<template>
+    <div class="bg-gray-100">
+        <div class="min-h-screen container mx-auto flex">
+            <nav class="p-3 border-r-2 border-l-2 border-gray-300">
+                <ApplicationLogo style="height: 50px" />
+            </nav>
+            <!-- Page Content -->
+            <main class="flex-1">
+                <slot />
+            </main>
+        </div>
+    </div>
+</template>
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { Head, Link } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <Head title="Welcome" />
+    <div class="min-h-screen bg-gray-100"></div>
+</template>
