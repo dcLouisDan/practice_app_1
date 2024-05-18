44154c3b2621381a3331505bf2c3ceca5a22dd75
Dan Louis Dela Cruz <louisdc002@gmail.com>
2024-05-18 22:18:29 +0800

    Add user name in nav and add logout button

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,20 +1,22 @@
 <script setup>
 import { ref } from "vue";
+import { Link, usePage } from "@inertiajs/vue3";
 import ApplicationLogo from "@/Components/ApplicationLogo.vue";
 import NavLink from "@/Components/NavLink.vue";
 import HomeIcon from "vue-material-design-icons/Home.vue";
 import MagnifyIcon from "vue-material-design-icons/Magnify.vue";
 import AccountIcon from "vue-material-design-icons/Account.vue";
 
-const showingNavigationDropdown = ref(false);
+const page = usePage();
+const user = page.props.auth.user;
 </script>
 
 <template>
     <div class="bg-gray-100">
         <div class="min-h-screen container mx-auto flex">
-            <nav class="p-3 border-r-2 border-gray-300">
+            <nav class="p-3 border-r-2 border-gray-300 flex flex-col">
                 <ApplicationLogo style="height: 50px" />
-                <div class="mt-4 flex flex-col sm:w-60">
+                <div class="mt-4 flex flex-col sm:w-60 flex-1">
                     <NavLink
                         :href="route('dashboard')"
                         class="items-center text-2xl flex gap-4"
@@ -26,15 +28,27 @@ const showingNavigationDropdown = ref(false);
                     <NavLink
                         :icon="MagnifyIcon"
                         class="items-center text-2xl flex gap-4"
+                        href="#"
                     >
                         Explore
                     </NavLink>
                     <NavLink
                         :icon="AccountIcon"
                         class="items-center text-2xl flex gap-4"
+                        href="#"
                     >
                         Profile
                     </NavLink>
+                </div>
+                <div class="content-end mb-5 flex flex-col">
+                    <div class="text-lg">{{ user.name }}</div>
+                    <Link
+                        method="post"
+                        class="text-sm text-gray-600 hover:text-gray-800 text-start"
+                        :href="route('logout')"
+                        as="button"
+                        >Logout</Link
+                    >
                 </div>
             </nav>
             <!-- Page Content -->
