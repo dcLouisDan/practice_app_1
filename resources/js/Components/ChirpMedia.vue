1ac58592a520e7624ebdbcfbbc756b3dac7d3e7a
Dan Louis Dela Cruz <louisdc002@gmail.com>
2024-06-03 16:30:50 +0800

    Create Mediaitem component and fix photo grids.

--- resources/js/Components/ChirpMedia.vue
+++ resources/js/Components/ChirpMedia.vue
@@ -1,4 +1,7 @@
 <script setup>
+import Modal from "./Modal.vue";
+import MediaItem from "./MediaItem.vue";
+
 const props = defineProps({
     media: Array,
 });
@@ -7,22 +10,38 @@ console.log(props.media);
 </script>
 
 <template>
-    <div class="flex justify-center">
-        <div v-if="media[0].media_type === 'video'">
-            <video controls :src="media[0].media_url" class="rounded-lg v-50" />
-        </div>
-        <div v-if="media.length === 1 && media[0].media_type === 'image'">
-            <img :src="media[0].media_url" alt="" class="rounded-lg v-50" />
+    <div class="flex justify-center max-w-lg">
+        <div v-if="media.length === 1" class="rounded-lg v-50 overflow-hidden">
+            <MediaItem :media="media[0]" />
         </div>
         <div
             v-if="media.length === 2 && media[0].media_type === 'image'"
-            class="flex rounded-lg overflow-hidden w-fit"
+            class="flex gap-1 rounded-lg overflow-hidden w-fit"
         >
-            <img
+            <MediaItem
                 v-for="image in media"
-                :src="image.media_url"
+                :media="image"
                 class="h-72 w-72 object-cover v-50"
             />
         </div>
+        <div
+            v-if="media.length === 3 && media[0].media_type === 'image'"
+            class="grid grid-cols-2 rounded-lg overflow-hidden w-fit h-96 gap-1"
+        >
+            <MediaItem :media="media[0]" class="object-cover v-50 row-span-2" />
+            <MediaItem :media="media[1]" class="object-cover v-50" />
+            <MediaItem :media="media[2]" class="object-cover v-50" />
+        </div>
+        <div
+            v-if="media.length === 4 && media[0].media_type === 'image'"
+            class="grid grid-cols-2 rounded-lg overflow-hidden w-fit h-96 gap-1"
+        >
+            <MediaItem
+                v-for="item in media"
+                :media="item"
+                class="object-cover v-50"
+            />
+        </div>
     </div>
+    <!-- <Modal :show="true"> </Modal> -->
 </template>
---
+++ resources/js/Components/MediaItem.vue
@@ -0,0 +1,23 @@
+<script setup>
+const props = defineProps({
+    media: Object,
+});
+
+console.log(props.media);
+</script>
+
+<template>
+    <div class="overflow-hidden flex items-center justify-center">
+        <img
+            :src="media.media_url"
+            v-if="media.media_type === 'image'"
+            class="object-cover w-96 h-96"
+        />
+        <video
+            controls
+            v-if="media.media_type === 'video'"
+            :src="media.media_url"
+            class="rounded-lg v-50"
+        />
+    </div>
+</template>
